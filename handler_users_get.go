97ca9e2ba1351789f586e535215e6d953ce2d137
viktor.k <viktor.kharts@gmail.com>
2024-03-03 20:45:52 +0100

    add auth package and middleware

--- handler_users_get.go
+++ handler_users_get.go
@@ -6,19 +6,6 @@ import (
 )
 
 func (c *apiConfig) usersGetHandler(w http.ResponseWriter, r *http.Request) {
-	header := r.Header.Get("Authorization")
-	if header == "" {
-		respondWithError(w, http.StatusUnauthorized, "No api key provided")
-		return
-	}
-	apiKey := strings.Split(header, " ")[1]
-	
-	user, err := c.DB.GetUserByApiKey(r.Context(), apiKey)
-	if err != nil {
-		respondWithError(w, http.StatusNotFound, "User not found")
-		return
-	}
-	
 	respondWithJson(w, http.StatusOK, databaseUserToUser(user))	
 }
 
